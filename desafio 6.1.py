1093180b75991c3eb32d422d0a47155b95abdff4
gustavosamistraro-312 <gustavo.samistraro@escola.pr.gov.br>
2026-03-18 08:47:03 -0300

    Create desafio 6.1.py

---
+++ desafio 6.1.py
@@ -0,0 +1,7 @@
+horas = int(input("Digite as horas: "))
+minutos = int(input("Digite os minutos: "))
+segundos = int(input("Digite os segundos: "))
+
+total_segundos = (horas * 3600) + (minutos * 60) + segundos
+
+print("Total em segundos:", total_segundos)
