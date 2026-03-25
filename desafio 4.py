316e0f18cd56508c2289fa17bd54b9c4a7e96557
gustavosamistraro-312 <gustavo.samistraro@escola.pr.gov.br>
2026-03-25 08:26:22 -0300

    Update desafio 4.py

--- desafio 4.py
+++ desafio 4.py
@@ -1,17 +1,25 @@
+# Entrada dos números
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
+
+# Entrada da operação
 operacao = input("Digite a operação (+, -, *, /): ")
 
+# Verifica qual operação será feita
 if operacao == "+":
     print("Resultado:", num1 + num2)
+
 elif operacao == "-":
     print("Resultado:", num1 - num2)
+
 elif operacao == "*":
     print("Resultado:", num1 * num2)
+
 elif operacao == "/":
     if num2 != 0:
         print("Resultado:", num1 / num2)
     else:
         print("Erro: divisão por zero")
+
 else:
     print("Operação inválida")
